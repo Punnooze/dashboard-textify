08cd57b80f1fdcae5fce44a1436ae9efb0ca4819
Punnoose Punnen <punnoose88@gmail.com>
2024-10-12 17:48:45 +0530

    analytics responsive

--- app/analytics/page.tsx
+++ app/analytics/page.tsx
@@ -13,8 +13,37 @@ export default function page() {
   return (
     <div className="bg-bgLightBlue rounded-xl p-[20px]">
       <p className="text-[36px] mb-[20px]">Analytics</p>
-      <div className="col-span-3">
-        <div className=" mb-[20px] grid grid-cols-4 gap-[20px]">
+      <div className=" mb-[20px] flex flex-col lg:grid  lg:grid-cols-4 gap-[20px]">
+        <div className="md:hidden lg:block bg-darkerBlue rounded-xl p-[20px]">
+          <p className="text-[22px] mb-[20px]">Age Group Distribution</p>
+          <DoughnutChart
+            dougnutData={[18, 15, 8, 2]}
+            doughnutLabels={['16-25', '26-35', '36-45', '45+']}
+          />
+          <div>
+            <div className="flex items-center mt-[20px]">
+              <div className="bg-chartYellow h-[10px] w-[10px] rounded-full mr-[10px]"></div>
+              <p>16 - 25 years </p>
+              <p className="ml-[5px] font-bold">18</p>
+            </div>
+            <div className="flex items-center">
+              <div className="bg-chartBlue h-[10px] w-[10px] rounded-full mr-[10px]"></div>
+              <p>26 - 35 years </p>
+              <p className="ml-[5px] font-bold">15</p>
+            </div>
+            <div className="flex items-center">
+              <div className="bg-chartGreen h-[10px] w-[10px] rounded-full mr-[10px]"></div>
+              <p>36 45 years </p>
+              <p className="ml-[5px] font-bold">8</p>
+            </div>
+            <div className="flex items-center">
+              <div className="bg-chartGreen h-[10px] w-[10px] rounded-full mr-[10px]"></div>
+              <p>45+ years </p>
+              <p className="ml-[5px] font-bold">2</p>
+            </div>
+          </div>
+        </div>
+        <div className="hidden md:flex lg:hidden">
           <div className="bg-darkerBlue rounded-xl p-[20px]">
             <p className="text-[22px] mb-[20px]">Age Group Distribution</p>
             <DoughnutChart
@@ -44,27 +73,13 @@ export default function page() {
               </div>
             </div>
           </div>
-          <div className="bg-darkerBlue rounded-xl col-span-3 flex flex-col justify-between p-[20px]">
-            <p className="text-[22px] mb-[20px]">Sales of Each Category</p>
-            <div className="h-full">
-              <LineChart />
-            </div>
-          </div>
-
-          <div className="h-[400px] bg-darkerBlue rounded-xl p-[20px] flex flex-col justify-between col-span-3">
-            <p className="text-[22px]">Profit from Each Category of products</p>
-            <div className="h-full">
-              <BarChart2 />
-            </div>
-          </div>
-
-          <div className="bg-darkerBlue rounded-xl p-[30px]">
+          <div className="ml-[20px] hidden md:block lg:hidden bg-darkerBlue rounded-xl p-[30px]">
             <p className="text-[24px] mb-[10px]">Sales Conversion Rate</p>
             <p className="text-[14px] text-lightGray ">
               (Amount of visitors who became customers)
             </p>
 
-            <p className="text-[90px] text-red">
+            <p className="text-[90px] md:text-[70px] xl:text-[90px] text-red">
               <CountUp
                 start={0.0}
                 end={3.6}
@@ -84,66 +99,106 @@ export default function page() {
               </p>
             </div>
           </div>
+        </div>
+        <div className="bg-darkerBlue rounded-xl md:col-span-3 flex flex-col justify-between p-[20px]">
+          <p className="text-[22px] mb-[20px]">Sales of Each Category</p>
+          <div className="h-full">
+            <LineChart />
+          </div>
+        </div>
 
-          <div className="grid col-span-1 gap-[20px]">
-            <div className="bg-darkerBlue p-[20px] rounded-xl">
-              <p className="text-[22px] mb-[40px]">Device Type</p>
-              <div className="flex items-center mb-[20px]">
-                <CountUp
-                  start={0}
-                  end={30}
-                  duration={1.5}
-                  suffix="%"
-                  className="text-[28px] mr-[10px] text-chartBlue"
-                />
-                <p>Mobile Devices</p>
-              </div>
-              <div className="flex items-center mb-[20px]">
-                <CountUp
-                  start={0}
-                  end={15}
-                  duration={1.5}
-                  suffix="%"
-                  className="text-[28px] mr-[10px] text-chartYellow"
-                />
-                <p>Tablet Devices</p>
-              </div>
-              <div className="flex items-center mb-[20px]">
-                <CountUp
-                  start={0}
-                  end={55}
-                  duration={1.5}
-                  suffix="%"
-                  className="text-[28px] mr-[10px] text-chartGreen"
-                />
-                <p>Desktop Devices</p>
-              </div>
+        <div className="h-[400px] bg-darkerBlue rounded-xl p-[20px] flex flex-col justify-between md:col-span-3">
+          <p className="text-[22px]">Profit from Each Category of products</p>
+          <div className="h-[300px] md:h-full">
+            <BarChart2 />
+          </div>
+        </div>
+
+        <div className="md:hidden lg:block bg-darkerBlue rounded-xl p-[30px]">
+          <p className="text-[24px] mb-[10px]">Sales Conversion Rate</p>
+          <p className="text-[14px] md:text-[12px] lg:text-[14px] text-lightGray ">
+            (Amount of visitors who became customers)
+          </p>
+
+          <p className="text-[90px] md:text-[60px] xl:text-[90px] text-red">
+            <CountUp
+              start={0.0}
+              end={3.6}
+              decimals={1} // To have one decimal place
+              duration={2} // Duration of the count-up animation (in seconds)
+              suffix="%"
+            />
+          </p>
+
+          <div className="flex">
+            <div className="flex px-[5px] items-center bg-red/25 rounded-md text-red">
+              <FaCaretDown />
+              <p className="text-[14px]">28%</p>
             </div>
-            <div className="bg-darkerBlue p-[20px] rounded-xl">
-              <p className="text-[22px] mb-[20px]">Abandoned Cart</p>
+            <p className="text-[14px] md:text-[12px] lg:text-[14px] ml-[5px]">
+              lesser conversion than last month
+            </p>
+          </div>
+        </div>
+
+        <div className="hidden lg:grid col-span-1 gap-[20px]">
+          <div className="bg-darkerBlue p-[20px] rounded-xl">
+            <p className="text-[22px] mb-[40px]">Device Type</p>
+            <div className="flex items-center mb-[20px]">
               <CountUp
-                start={0.0}
-                end={11.3}
-                decimals={1}
+                start={0}
+                end={30}
                 duration={1.5}
                 suffix="%"
-                className="text-[56px]"
+                className="text-[28px] mr-[10px] text-chartBlue"
               />
+              <p>Mobile Devices</p>
+            </div>
+            <div className="flex items-center mb-[20px]">
+              <CountUp
+                start={0}
+                end={15}
+                duration={1.5}
+                suffix="%"
+                className="text-[28px] mr-[10px] text-chartYellow"
+              />
+              <p>Tablet Devices</p>
+            </div>
+            <div className="flex items-center mb-[20px]">
+              <CountUp
+                start={0}
+                end={55}
+                duration={1.5}
+                suffix="%"
+                className="text-[28px] mr-[10px] text-chartGreen"
+              />
+              <p>Desktop Devices</p>
             </div>
           </div>
-          <div className="col-span-3 bg-darkerBlue p-[20px] rounded-xl">
-            <p className="text-[22px] mb-[20px]">Products with Low Stock</p>
-            <DataTable columns={columns2} data={stockData} />
+          <div className="bg-darkerBlue p-[20px] rounded-xl">
+            <p className="text-[22px] mb-[20px]">Abandoned Cart</p>
+            <CountUp
+              start={0.0}
+              end={11.3}
+              decimals={1}
+              duration={1.5}
+              suffix="%"
+              className="text-[56px]"
+            />
           </div>
+        </div>
+        <div className="md:col-span-4 lg:col-span-3 bg-darkerBlue p-[20px] rounded-xl">
+          <p className="text-[22px] mb-[20px]">Products with Low Stock</p>
+          <DataTable columns={columns2} data={stockData} />
+        </div>
 
-          <div className="col-span-4 grid grid-cols-4 gap-[20px]">
-            <div className="bg-darkerBlue rounded-xl p-[20px] col-span-4">
-              <p className="text-[22px] mb-[20px]">Top Selling Products</p>
-              <div className="grid grid-cols-5 overflow-auto gap-[20px]">
-                {products.map((item, index) => (
-                  <Card key={index} product={item} />
-                ))}
-              </div>
+        <div className="col-span-4 grid grid-cols-4 gap-[20px]">
+          <div className="bg-darkerBlue rounded-xl p-[20px] col-span-4">
+            <p className="text-[22px] mb-[20px]">Top Selling Products</p>
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 overflow-auto gap-[20px]">
+              {products.map((item, index) => (
+                <Card key={index} product={item} />
+              ))}
             </div>
           </div>
         </div>
--- components/BarChart2.tsx
+++ components/BarChart2.tsx
@@ -1,5 +1,5 @@
 'use client';
-import {  Bar } from 'react-chartjs-2';
+import { Bar } from 'react-chartjs-2';
 import {
   Chart as ChartJS,
   LineElement,
@@ -12,7 +12,7 @@ import {
 } from 'chart.js';
 import { productData } from '../utils/data';
 
-// Register the necessary chart elements for a line chart
+// Register the necessary chart elements
 ChartJS.register(
   LineElement,
   BarElement,
@@ -24,44 +24,48 @@ ChartJS.register(
 );
 
 export default function BarChart2() {
-  // Format the product performance data for the line chart
-
+  // Format the product performance data for the bar chart
   const data2 = {
     labels: productData.map((item) => item.month), // Months for the x-axis
     datasets: [
       {
         label: 'Oversized T-shirt Sales',
-        data: productData.map((item) => item.oversizedTshirt.profit), // Oversized T-shirt sales data
+        data: productData.map((item) => item.oversizedTshirt.profit),
         borderColor: 'rgba(54, 162, 235, 1)',
         backgroundColor: 'rgba(54, 162, 235, 0.5)',
-        tension: 0, // Smooth the line
+        tension: 0,
         fill: false,
       },
       {
         label: 'Shirt Sales',
-        data: productData.map((item) => item.shirt.profit), // Shirt sales data
+        data: productData.map((item) => item.shirt.profit),
         borderColor: 'rgba(255, 99, 132, 1)',
         backgroundColor: 'rgba(255, 99, 132, 0.5)',
-        tension: 0, // Smooth the line
+        tension: 0,
         fill: false,
       },
       {
         label: 'Pants Sales',
-        data: productData.map((item) => item.pants.profit), // Pants sales data
+        data: productData.map((item) => item.pants.profit),
         borderColor: 'rgba(75, 192, 192, 1)',
         backgroundColor: 'rgba(75, 192, 192, 0.5)',
-        tension: 0, // Smooth the line
+        tension: 0,
         fill: false,
       },
       {
         label: 'Hoodies Sales',
-        data: productData.map((item) => item.hoodies.profit), // Hoodies sales data
+        data: productData.map((item) => item.hoodies.profit),
         borderColor: 'rgba(153, 102, 255, 1)',
         backgroundColor: 'rgba(153, 102, 255, 0.5)',
-        tension: 0, // Smooth the line
+        tension: 0,
         fill: false,
       },
     ],
+  };
+
+  // Function to determine the legend position based on screen size
+  const getLegendPosition = (): 'top' | 'right' => {
+    return window.innerWidth < 768 ? 'top' : 'right'; // Adjust the width as per your breakpoint
   };
 
   const optionsBar = {
@@ -69,21 +73,21 @@ export default function BarChart2() {
     maintainAspectRatio: false,
     plugins: {
       legend: {
-        position: 'right' as const, // Legend on the right
+        position: getLegendPosition(), // Set legend position dynamically
         labels: {
-          color: 'rgba(209, 213, 219, 1)', // Legend text color to rgba(209, 213, 219, 1)
+          color: 'rgba(209, 213, 219, 1)',
         },
       },
     },
     scales: {
       x: {
-        beginAtZero: true, // Start x-axis at 0
+        beginAtZero: true,
         ticks: {
-          color: 'rgba(209, 213, 219, 1)', // X-axis labels text color to rgba(209, 213, 219, 1)
+          color: 'rgba(209, 213, 219, 1)',
         },
       },
       y: {
-        beginAtZero: true, // Start y-axis at 0
+        beginAtZero: true,
         title: {
           display: true,
           text: 'Sales and Profit',
@@ -95,6 +99,7 @@ export default function BarChart2() {
       },
     },
   };
+
   return (
     <div className="p-[10px] h-[100%] w-[100%]">
       <Bar data={data2} options={optionsBar} />
